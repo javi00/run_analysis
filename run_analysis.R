198c1bfa21d988b7bee13fc0423125b4fca7e861
javi00 <jvevallejos@gmail.com>
2020-08-14 22:02:43 -0400

    finish

--- run_analysis.R
+++ run_analysis.R
@@ -30,14 +30,36 @@ total_train <- cbind(subject_train,y_train,x_train)
 #merge to unique dataset with test and train
 total <- rbind(total_test,total_train)
 
+#activity 
+t1 <- total[total$label == 1,]
+t2 <- total[total$label == 2,]
+t3 <- total[total$label == 3,]
+t4 <- total[total$label == 4,]
+t5 <- total[total$label == 5,]
+t6 <- total[total$label == 6,]
+
 #calculate mean and standard deviation
-mean <- sapply(total, mean)
-str  <- sapply(total, sd)
+mean1 <- sapply(t1, mean)
+str1  <- sapply(t1, sd)
+mean2 <- sapply(t2, mean)
+str2  <- sapply(t2, sd)
+mean3 <- sapply(t3, mean)
+str3  <- sapply(t3, sd)
+mean4 <- sapply(t4, mean)
+str4  <- sapply(t4, sd)
+mean5 <- sapply(t5, mean)
+str5  <- sapply(t5, sd)
+mean6 <- sapply(t6, mean)
+str6  <- sapply(t6, sd)
 
 # to finish I created only one dataset with mean and standard deviation
-result <- rbind(mean,str)
+result <- rbind(mean1,str1,mean2,str2,mean3,str3,mean4,str4,mean5,str5,mean6,str6)
+act<-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt", sep = "")
+
+rownames(result) <- c(act[,2],act[,2])
+
 result <-  as.data.frame(result)
 # write a  table with the results
 
-write.table(result,"C:/Users/javi_/Documents/R/run_analysis/result.txt", sep = " ",row.name=FALSE)
+write.table(result,"C:/Users/javi_/Documents/R/run_analysis/result.txt", sep = " ")
 
