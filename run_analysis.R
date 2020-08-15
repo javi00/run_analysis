c356339606afe73f1820d583d6a259336d1aa0fe
javi00 <jvevallejos@gmail.com>
2020-08-15 11:31:11 -0400

    until 4 point

--- run_analysis.R
+++ run_analysis.R
@@ -1,35 +1,49 @@
 library(dplyr)
 
-# load the test data sets 
+# 1. Merge test and train data set
+## Load the test dataset 
 x_test <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt", sep = "")
 y_test <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt", sep = "")
 subject_test <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt", sep = "")
-
-# Here I put the name to each column
+## Name to each column with 
+features <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt", sep = "")
 colnames(x_test) <- features[,2]
 colnames(y_test) <- "activity"
 colnames(subject_test) <- "sample"
-
-#then I merge all test data sets
-total_test <- cbind(subject_test,y_test,x_test)
-
-# load the train data sets 
+# Load the train dataset 
 x_train <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt", sep = "")
 y_train <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt", sep = "")
 subject_train <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt", sep = "")
-
 # here I put the name to each column
 colnames(x_train) <- features[,2]
 colnames(y_train) <- "activity"
 colnames(subject_train) <- "sample"
 
-#then I merge all train data sets
-total_train <- cbind(subject_train,y_train,x_train)
 
-#merge to unique dataset with test and train
+# 2. Extracts only the measurements on the mean and standard deviation
+## Name to each activity 
+act_label <-  read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt", sep = "")
+## select only the mean and std colums in TEST
+xmst <- x_test[grep("mean()|std()", names(x_test))]
+## it is merging all  test data sets
+total_test <- cbind(subject_test,y_test,xmst)
+
+## select only the mean and std colums in TRAIN
+xmstr <- x_train[grep("mean()|std()", names(x_train))]
+## it is merging all train data sets
+total_train <- cbind(subject_train,y_train,xmstr)
+# merge to unique dataset with test and train
 total <- rbind(total_test,total_train)
 
-#activity 
+#3. descriptive activity names
+## here arrange and names each activity
+total <- arrange(total, activity)
+
+#4 Appropriately labels the data set with descriptive variable names
+total$activity <- act_label[total$activity,2]
+
+#5. Creates a second, independent tidy data set with the average of each variable 
+
 t1 <- total[total$label == 1,]
 t2 <- total[total$label == 2,]
 t3 <- total[total$label == 3,]
@@ -38,7 +52,7 @@ t5 <- total[total$label == 5,]
 t6 <- total[total$label == 6,]
 
 #calculate mean and standard  deviation
-mean1 <- sapply(t1, mean)
+mean1 <- sapply(total, mean)
 str1  <- sapply(t1, sd)
 mean2 <- sapply(t2, mean)
 str2  <- sapply(t2, sd)
